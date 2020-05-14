de014d8efb8bc6d33aa0de7d92985fde407f8eb4
emreharman <emre.harman@ogr.sakarya.edu.tr>
2020-05-14 05:10:54 +0300

    Veri Tabanı Kullanarak Login Senaryosu Signed-off-by: emreharman <emre.harman@ogr.sakarya.edu.tr>

---
+++ Proje5/02/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace _02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(79, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(79, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Parola";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(232, 148);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(289, 28);
+            this.txtKullaniciAdi.TabIndex = 2;
+            // 
+            // txtParola
+            // 
+            this.txtParola.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtParola.Location = new System.Drawing.Point(232, 214);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.Size = new System.Drawing.Size(289, 28);
+            this.txtParola.TabIndex = 3;
+            this.txtParola.UseSystemPasswordChar = true;
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGiris.Location = new System.Drawing.Point(244, 300);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(157, 52);
+            this.btnGiris.TabIndex = 4;
+            this.btnGiris.Text = "GİRİŞ";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 441);
+            this.Controls.Add(this.btnGiris);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Button btnGiris;
+    }
+}
+
---
+++ Proje5/02/Form1.cs
@@ -0,0 +1,124 @@
+﻿/****************************************************************************
+**																		   	
+**				YAZAN.............: Emre Harman							   	
+**				TARİH.............: 12.05.2020	
+**
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Connection stringimiz ve bağlantımız
+        const string veriYolu = @"Data Source=DESKTOP-QH1AV1B; Initial Catalog=dboCalisma2; Integrated Security=True";
+        SqlConnection baglanti = new SqlConnection(veriYolu);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            //Kullanıcının girdiği değerler
+            string kulAd = txtKullaniciAdi.Text;
+            string parola = txtParola.Text;
+            
+            //Bağlantımızı açtık
+            baglanti.Open();
+            string sorgu = "SELECT kullaniciAdi,parola,durum,girisHak FROM T_Kullanicilar";//Sordu cümlemizi yazdık.
+            SqlCommand komut = new SqlCommand(sorgu,baglanti);//Baglantimizi kurduk.Simdi sorguyu calistirip veriyi almamiz lazim.
+            //Komutu  gerçekleştirdik.
+            SqlDataReader dr = komut.ExecuteReader();
+            //Tablomuzu satır satır okuyup verilemizi kontrol edeceğiz.
+            while (dr.Read())
+            {
+                //Eğer alanlardan biri ya da ikisi birden boş ise
+                if(kulAd=="" || parola == "")
+                {
+                    MessageBox.Show("Kullanıcı adı ya da Parola boş bırakılamaz.");
+                    baglanti.Close();
+                    break;
+                }
+               
+                //Kullanıcı adı ve parola doğru girilmiş ve aynı zamanda kullanıcının durumu aktif ise form2'ye yönlendir.
+                //Kullanıcının başarılı giriş yaptığı için her ihtimale karşı giriş hakkını yeniden 3 yap.
+                if (dr["kullaniciAdi"].ToString() == kulAd && dr["parola"].ToString()==parola && dr["durum"].ToString()=="aktif")
+                {
+                    Form2 form2 = new Form2();
+                    form2.Show();
+                    this.Close();
+                    dr.Close();
+                    komut.CommandText = "UPDATE T_Kullanicilar SET girisHak=3 WHERE kullaniciAdi='"+kulAd+"' ";
+                    komut.ExecuteNonQuery();
+                    baglanti.Close();
+                    break;
+                }
+                //Kullanıcı adı ve parola doğru ancak durumu kilitli ise hata ver, tekrar girişi engelle.
+                if(dr["kullaniciAdi"].ToString() == kulAd && dr["parola"].ToString() == parola && dr["durum"].ToString() == "kilitli")
+                {
+                    MessageBox.Show("Hesabınız kilitlidir. Lütfen sistem yöneticinizle iletişime geçin.");
+                    btnGiris.Enabled = false; 
+                    baglanti.Close();
+                    break;
+                }
+                //Kullanıcı adı ya da parola yanlışsa
+                if(dr["kullaniciAdi"].ToString()!=kulAd || dr["parola"].ToString() != parola)
+                {
+                    //KUllanıcının durumu kilitli ise girişini engelle.
+                    if (dr["durum"].ToString() == "kilitli")
+                    {
+                        MessageBox.Show("Hesabınız kilitlidir. Lütfen sistem yöneticinizle iletişime geçin.");
+                        btnGiris.Enabled = false;
+                        baglanti.Close();
+                        break;
+                    }
+                    dr.Close();//Yeni komut yazmak için açık olan DataReader'ı kapatmamız lazım.
+                    //Kullanıcının giriş hakkını 1 azalt, veri tabanında güncelle
+                    komut.CommandText = "UPDATE T_Kullanicilar SET girisHak=girisHak-1";
+                    komut.ExecuteNonQuery();
+                    komut.CommandText = "SELECT * FROM T_Kullanicilar WHERE kullaniciAdi='"+kulAd+"'";
+                    dr = komut.ExecuteReader();//Güncel bilgiyi tekrar oku.
+                    //Güncel bilgiyi okuyoruz.
+                    while (dr.Read())
+                    {
+                        //Kullanıcının kalan hakkını bildirdik.
+                        MessageBox.Show("Kullanıcı adınız ya da Parolanız yanlış. Kalan hak "+dr["girisHak"].ToString());
+                        //giriş hakkı bittiyse durumunu kilitli yaparak girişini engelliyoruz.
+                        if (dr["girisHak"].ToString() == "0")
+                        {
+                            dr.Close();
+                            komut.CommandText = "UPDATE T_Kullanicilar SET durum='kilitli'";
+                            komut.ExecuteNonQuery();
+                            MessageBox.Show("Hesabınız kilitlenmiştir. Lütfen sistem yöneticinizle iletişime geçin.");
+                            btnGiris.Enabled = false;
+                            txtKullaniciAdi.Clear();
+                            txtParola.Clear();
+                            baglanti.Close();
+                            break;
+                        }
+                    }
+                    txtKullaniciAdi.Clear();
+                    txtParola.Clear();
+                    baglanti.Close();
+                    break;
+                }
+                
+            }
+        }
+    }
+}
